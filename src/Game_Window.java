63a16db6f04faf1718bb5587a82b00eb4236a51a
michalbohn <michal.bohn@gmail.com>
2016-04-09 22:17:01 +0200

    2

--- src/Game_Window.java
+++ src/Game_Window.java
@@ -1,13 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Klasa tworzaca okno gry
+ */
+
 public class Game_Window extends JFrame {
 
-    //Inicjacja obiektow
+    /**Inicjacja obiektow*/
     Small_GMenu smallgmenu;
     Game_Field gamefield;
 
-    //Konstruktor glownego okna gry
+    /**Konstruktor glownego okna gry*/
     public Game_Window(){
         init_panels();
         ops_Game_Window();
@@ -15,38 +19,38 @@ public class Game_Window extends JFrame {
         ops_Game_Field();
     }
 
-    //Metoda tworzaca panele w glownym oknie gry
+    /**Metoda tworzaca panele w glownym oknie gry*/
     private void init_panels(){
         smallgmenu = new Small_GMenu();
         gamefield = new Game_Field();
     }
 
-    //Metoda opisujaca opcje menu gry
+    /**Metoda opisujaca opcje menu gry*/
     private void ops_Small_GMenu(){
         smallgmenu.setBackground(Color.WHITE);
         smallgmenu.setVisible(true);
     }
 
-    //Metoda opisujaca opcje pola gry
+    /**Metoda opisujaca opcje pola gry*/
     private void ops_Game_Field(){
         gamefield.setVisible(true);
     }
 
-    //Metoda opisujaca opcje glownego okna gry
+    /**Metoda opisujaca opcje glownego okna gry*/
     private void ops_Game_Window(){
-        //Tytul glownego okna gry
+        /**Tytul glownego okna gry*/
         setTitle("Lunar Lander");
-        //Rozmiar startowy glownego okna gry
+        /**Rozmiar startowy glownego okna gry*/
         setSize(500,500);
-        //Minimalny rozmiar glownego okna gry
+        /**Minimalny rozmiar glownego okna gry*/
         setMinimumSize(new Dimension(500,500));
-        //Rozmieszczenie paneli w glownym oknie gry
+        /**Rozmieszczenie paneli w glownym oknie gry*/
         setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
         add(smallgmenu);
         add(gamefield);
-        //ustawia okno na srodku ekranu
+        /**ustawia okno na srodku ekranu*/
         setLocationRelativeTo(null);
-        //Domyslna operacja po zamknieciu okna gry - zamkniecie aplikacji
+        /**Domyslna operacja po zamknieciu okna gry - zamkniecie aplikacji*/
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //setIconImage(new ImageIcon("C:/Users/Jakub/OneDrive/ELKA/PROZE/PROJEKT/images/moon_icon.jpg").getImage());
