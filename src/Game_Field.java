af5afac898e4516e742155099e1093a483bc633c
michalbohn <michal.bohn@gmail.com>
2016-04-09 20:51:05 +0200

    2

--- src/Game_Field.java
+++ src/Game_Field.java
@@ -6,7 +6,7 @@ public class Game_Field extends JPanel {
 
     public static final String Time = "Time";
     public static final String Score = "Score";
-    public static final String chuj = "chuj";
+    public static final String chuj = "chuj ci w dupe";
     public Game_Field(){
 
     }
