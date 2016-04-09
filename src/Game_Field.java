525eff231f6705263a3d9be0d520ebf7754d9c69
jakobian <gawlik.kuba@gmail.com>
2016-04-09 21:19:07 +0200

    1

--- src/Game_Field.java
+++ src/Game_Field.java
@@ -6,9 +6,6 @@ public class Game_Field extends JPanel {
 
     public static final String Time = "Time";
     public static final String Score = "Score";
-    public static final String S = "proba";
-    public static final String nadzieja = "nadzieja";
-    public static final String Sproba2 = "proba227";
 
 
     public Game_Field(){
