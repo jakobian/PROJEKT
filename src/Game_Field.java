2ff951640afbdd054b9d76d8b2e419c2af54eef9
jakobian <gawlik.kuba@gmail.com>
2016-04-09 21:04:59 +0200

    1

--- src/Game_Field.java
+++ src/Game_Field.java
@@ -6,9 +6,7 @@ public class Game_Field extends JPanel {
 
     public static final String Time = "Time";
     public static final String Score = "Score";
-    public static final String chuj = "chuj";
-    public static final String chdsduj = "chudsj";
-    public static final String c = "c";
+
     public Game_Field(){
 
     }
