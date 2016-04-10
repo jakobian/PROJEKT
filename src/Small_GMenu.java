9206c0d1668bb77c5938952397e2285d5738920f
jakobian <gawlik.kuba@gmail.com>
2016-04-10 17:23:16 +0200

    1

--- src/Small_GMenu.java
+++ src/Small_GMenu.java
@@ -5,23 +5,36 @@ import java.awt.*;
 /** Klasa menu okna gry */
 public class Small_GMenu extends JPanel {
 
-    /** Pola do przechowywania nazw przyciskow menu okna gry */
+    /**
+     * Pola przechowujace nazwe przycisku "Menu" wyswietlanego w menu okna gry
+     */
     public final static String Menu = "Menu";
+    /**
+     * Pola przechowujace nazwe przycisku "Pause" wyswietlanego w menu okna gry
+     */
     public final static String Pause = "Pause";
 
-    /** Pole inicjujace przyciki menu okna gry */
+    /**
+     * Pole inicjujace przycik "Menu" okna gry
+     */
     JButton menubutton;
+    /**
+     * Pole inicjujace przycik "Pause" okna gry
+     */
     JButton pausebutton;
 
-    /** Konstruktor menu okna gry */
+
+    /**
+     * Konstruktor menu okna gry
+     */
     public Small_GMenu(){
         createbutton();
-        //setDimensionButton(70,20);
     }
 
-    /** Metoda tworzaca przyciski menu okna gry */
+    /**
+     * Metoda tworzaca przyciski menu okna gry
+     */
     private void createbutton(){
-       //this.setLayout(null);
        this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
 
         menubutton = new JButton(Menu);
@@ -30,10 +43,10 @@ public class Small_GMenu extends JPanel {
         add(pausebutton);
     }
 
-    /** Metoda ustawiajaca wielkosc przyciskow menu okna gry */
+    /**
+     *  Metoda ustawiajaca wielkosc przyciskow menu okna gry
+     */
     public void setDimensionButton(int width, int height){
-        //menubutton.setBounds(0,0,width,height);
-        //pausebutton.setBounds(width,0,width,height);
         menubutton.setPreferredSize(new Dimension(width, height));
         pausebutton.setPreferredSize(new Dimension(width, height));
     }
