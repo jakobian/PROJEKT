c8542ffbd9c9b8628a31b463243cb74e9130e001
jakobian <gawlik.kuba@gmail.com>
2016-04-11 21:05:16 +0200

    1

--- src/DrawUniverse.java
+++ src/DrawUniverse.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 
 /**
@@ -11,8 +15,31 @@ import java.awt.*;
  * Klasa definujaca wszechswiat - podloze, grawitacje...
  */
 public class DrawUniverse {
-    private double gravity;
 
+
+    private double gravity;
+    private Point[] area;
+
+
+    /**
+     * Konstruktor klasy DrawUniverse
+     */
+    DrawUniverse() throws IOException{
+        File file = new File("resources/Area.properties");
+        FileInputStream fileInput = new FileInputStream(file);
+        Properties properties = new Properties();
+        properties.load(fileInput);
+        fileInput.close();
+    }
+
+    private void createArea(){
+        area = new Point[Integer.parseInt(properties.getProperty(""))];
+        for(int i = 0; i<area.length;++i){
+            area[i] = new Point(Integer.parseInt(properties.getProperty("")),
+                    Integer.parseInt(properties.getProperty("")));
+        }
+
+    }
 
 
 }
