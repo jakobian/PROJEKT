826eeb79832c3689617ecc96956991c36966e3ee
jakobian <gawlik.kuba@gmail.com>
2016-04-12 23:02:32 +0200

    1

--- src/GameField.java
+++ src/GameField.java
@@ -53,49 +53,35 @@ public class GameField extends JPanel{
         }
     }
 
-    private Dimension initSize = new Dimension(500,500);
+    private Dimension  initSize = new Dimension(500,500);
 
     /**
      * Metoda rysujaca podloze
      * @param g
-     * @param point_x
-     * @param point_y
+     * @param point_xc
+     * @param point_yc
      */
-    private void drawArea(Graphics g, int[] point_x, int[] point_y){
+    private void drawArea(Graphics g, int[] point_xc, int[] point_yc){
 
-        double xDim = getWidth();
-        double yDim = getHeight();
-        double xRatio = xDim/500;
-        double yRatio = yDim/500;
-
-        int total_number_points = point_x.length;
-        current_point_x = new int[total_number_points];
-        current_point_y = new int[total_number_points];
-        for (int i = 0; i < total_number_points; ++i) {
-            current_point_x[i] = (int) xRatio*point_x[i];
-            current_point_y[i] = (int) yRatio*point_y[i];
-        }
-
-        g.fillPolygon(point_x, point_y, point_x.length);
-    }
-
-    private int[] getAreaPointX(){
-        return point_x;
-    }
-
-    private int[] getAreaPointY(){
-        return point_y;
+        g.fillPolygon(point_xc, point_yc, point_x.length);
     }
 
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         //Graphics2D g2d = (Graphics2D) g;
-
-
-
+        setBorder(BorderFactory.createLineBorder(Color.red));
+        double xRatio = getWidth()/(double)480;
+        double yRatio = getHeight()/(double)480;
+        int total_number_points = point_x.length;
+        current_point_x = new int[total_number_points];
+        current_point_y = new int[total_number_points];
+        System.out.println(getWidth());
+        for (int i = 0; i < total_number_points; ++i) {
+            current_point_x[i] = (int)(xRatio*point_x[i]);
+            current_point_y[i] = (int)(yRatio*point_y[i]);
+        }
+        drawArea(g, current_point_x, current_point_y);
         g.setColor(Color.gray);
-        drawArea(g, getAreaPointX(), getAreaPointY());
     }
 }
-
